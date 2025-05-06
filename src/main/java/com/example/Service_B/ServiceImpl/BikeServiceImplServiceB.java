9945bfa917361e7956c4be8fa4a5335e1401e2d2
Elangovan P <V00681@VERINITE1.local>
2025-05-06 15:38:35 +0530

    Exchange Added in Rest Template

--- src/main/java/com/example/Service_B/ServiceImpl/BikeServiceImplServiceB.java
+++ src/main/java/com/example/Service_B/ServiceImpl/BikeServiceImplServiceB.java
@@ -2,6 +2,9 @@ package com.example.Service_B.ServiceImpl;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.info.ProjectInfoProperties.Git;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import com.example.Service_B.ServiceInter.BikeServiceInterServiceB;
@@ -33,13 +36,16 @@ public class BikeServiceImplServiceB implements BikeServiceInterServiceB {
 		
 		log.info("Bike saved in DB: {}", bikeDtoB);
 
-		BikeDtoB bikeResponse =restTemplate.postForEntity("http://localhost:8082/saveBikeA", bikeDtoB, BikeDtoB.class).getBody();
-		
-		log.info("Response received from Service A: {}", bikeResponse);
+		ResponseEntity<BikeDtoB> response = restTemplate.exchange(
+			    "http://service_a:8082/saveBikeA",
+			    HttpMethod.POST,
+			    bikeDtoB,
+			    BikeDtoB.class
+			);		
+		log.info("Response received from Service A: {}", response);
 
-		return bikeResponse;
+		return response.getBody();
 	}
-	
 	public BikeDtoB convertEntityToDto(BikeEntityB bikeEntityB) {
 		return modelMapper.map(bikeEntityB, BikeDtoB.class);
 	}
